1f3e4cba7ac08137728991f620d7596e28d26a11
Mariano Cereda <gmarianocereda@gmail.com>
2021-04-23 09:18:35 -0300

    Codigo terminado - Para que solo tome en cuenta los puntos necesarios, plantee una recursividad de nodos padres desde el fin hasta llegar al inicio

---
+++ TP1/Ejercicio 2/Node.py
@@ -0,0 +1,24 @@
+
+class Node:
+    def __init__(self,value,parent):
+        self.value = value
+        self.parent = parent
+        self.visited = 0
+        self.neighboors = []
+        self.fn = 0
+
+    def find_neighboors(self,shelves,goal,all_nodes):
+        ##self.neighboors = []
+        ri = self.value[0] #Coordenada x del punto actual
+        ci = self.value[1] #Coordenada y
+        self.neighboors = [[ri,ci+1],[ri,ci-1],[ri+1,ci],[ri-1,ci]]
+        to_remove = []
+        for i in self.neighboors:
+            if (i[0]<0) or (i[1]<0): #Guarada en un vector aquellos vecinos que tienen coordenadas [x,y] negativas (es decir, que no pertenecen a la matriz)
+                to_remove.append(i)
+            elif(i in shelves)and(i != goal): #Guarda en un vector los vecinos que son estanterías, para luego elminarlos. Así en el vector neighboors solo nos quedan vecinos por los cuales se puede circular (pasillos).
+                to_remove.append(i)
+        
+        for i in range(len(to_remove)): #Elimina los valores que en el for anterior guardamos en el vector "to_remove"
+            self.neighboors.remove(to_remove[i])
+
--- TP1/Ejercicio 2/ej_02.py
+++ TP1/Ejercicio 2/ej_02.py
@@ -1,6 +1,7 @@
 import numpy as np
 from Layout import *
 from Astar import *
+from Node import *
 
 if __name__ == "__main__":
     cols = 16
@@ -10,17 +11,33 @@ if __name__ == "__main__":
     halls = layout.create_halls()
     shelves = layout.create_shelves()
     init = [0,0]
-    goal = [14,11]
-    astar = Astar(init,goal,mapxy,shelves,halls)
+    goal = [15,15]
+    node_init = Node(init,None)
+    node_goal = Node(goal,None)
+    astar = Astar(init,goal,shelves)
+    current_node = node_init
+    all_nodes = []
+    first_it = False
     while astar.check():
-        astar.find_neighboors()
-        astar.gn()
-        astar.hn()
-        astar.fn()
-        astar.select_minimum()
-    print(astar.mapm)
-    print(astar.way)
+        current_node.find_neighboors(shelves,goal,all_nodes)
+        for i in range(len(current_node.neighboors)):
+            if current_node.neighboors[i] not in all_nodes:
+                all_nodes.append(Node(current_node.neighboors[i],current_node))
+            else:
+                for j in all_nodes:
+                    if(j.value == current_node.neighboors[i]):
+                        indice = all_nodes.index(j)
+                all_nodes.append(j)
+        current_node.visited = 1
+        astar.gn(current_node.neighboors)
+        astar.hn(current_node.neighboors)
+        astar.fn(current_node.neighboors)
+        current_node = astar.select_minimum(current_node,all_nodes)
+    astar.clean_way(mapxy)
     print(astar.count_gn)
+    
+    
+
     #astar.clean_way(rows,cols)
   
     
