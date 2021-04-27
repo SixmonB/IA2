460583b86b05bddbac0047b43573a02ccc0cd438
Zaratustra <65260383+matiasbertani@users.noreply.github.com>
2021-04-27 10:36:15 -0300

    borrado prueba

--- TP1/Ejercicio_3/hola.py
+++
@@ -1,8 +0,0 @@
-import sys  
-from pathlib import Path  
-file = Path(__file__). resolve()  
-package_root_directory = file.parents [1]  
-sys.path.append(str(package_root_directory))
-from Ejercicio_2 import ej_02
-
-print('hola')
