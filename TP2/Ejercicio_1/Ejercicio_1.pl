aa9b3d59cd60ef107c635f721ea182ef13a18a4c
Zaratustra <65260383+matiasbertani@users.noreply.github.com>
2021-06-25 09:29:16 -0300

    instanciacion valvula

--- TP2/Ejercicio_1/Ejercicio_1.pl
+++ TP2/Ejercicio_1/Ejercicio_1.pl
@@ -7,18 +7,21 @@
 
 
 /* Axiomas (invariantes del dominio) */
+action(X):- verificar(X) ;
+            writeln('La Valvula de seguridad funciona correctamente.').
 
 
 % Rama IZQUIERDA
 verificar(valvula_1) :-  
     
     thickness( valvula_1, Espesor ), threshold(valvula_1, Limite), Espesor < Limite , writeln('Equipment status will be reported to the Technical Inspection Unit immediately.');
-    thickness( valvula_1, Espesor ), threshold(valvula_1, Limite), Espesor >= Limite , writeln('The condition of the equipment is suitable');
+    % thickness( valvula_1, Espesor ), threshold(valvula_1, Limite), Espesor >= Limite , writeln('The condition of the equipment is suitable');
+
     
 
     (   thickness( valvula_1, desconocido ), 
         (   
-            ( estado(valvula_components_having_effects, no ), writeln('Verificar Thickness valve') );
+            ( estado(valvula_components_having_effects, no ), writeln('VERIFICAR COMPONENTE: Thickness valve') );
             verificar( valvula_components_having_effects )
         ) 
                 
@@ -26,20 +29,20 @@ verificar(valvula_1) :-
 
 verificar(safety_valve_components_having_effects) :-  
 
-    estado( safety_valve_components_having_effects, no ), writeln('Coordination is required in order to render and color the equipment.');   
+    estado( safety_valve_components_having_effects, no ), writeln('ACCION NECESARIA: Coordination is required in order to render and color the equipment.');   
 
-    estado( safety_valve_components_having_effects, desconocido ), writeln('Verificar safety valve components having effects').
+    estado( safety_valve_components_having_effects, desconocido ), writeln('VERIFICAR COMPONENTE: safety valve components having effects').
 
 
 
 % Rama CENTRAL izquierda
 verificar(piloto) :- 
-    estado(piloto, ok), writeln('Set the safety valve according to the instauctions');
-    estado(piloto, no), writeln('Pilot full service and reinstallation');
+    estado(piloto, ok), writeln('ACCION NECESARIA: Set the safety valve according to the instauctions');
+    estado(piloto, no), writeln('ACCION NECESARIA: Pilot full service and reinstallation');
     (
         estado(piloto, desconocido), 
         (
-        (estado(leakage_prevention_between_sit_and_orifice, ok), writeln('Verificar estado de Piloto'));
+        (estado(leakage_prevention_between_sit_and_orifice, ok), writeln('VERIFICAR COMPONENTE: estado de Piloto'));
         verificar(leakage_prevention_between_sit_and_orifice)
         )
     
@@ -48,28 +51,28 @@ verificar(piloto) :-
 
 
 verificar(leakage_prevention_between_sit_and_orifice) :- 
-                estado(leakage_prevention_between_sit_and_orifice, no), writeln('Replace Sit and Orifice and put the safety valve into circuit') ; 
+                estado(leakage_prevention_between_sit_and_orifice, no), writeln('ACCION NECESARIA: Replace Sit and Orifice and put the safety valve into circuit') ; 
                 estado(leakage_prevention_between_sit_and_orifice, desconocido), 
-                ((estado(safety_valve_spring, ok), writeln('Verificar leakage prevention between sit and orifice')); 
+                ((estado(safety_valve_spring, ok), writeln('VERIFICAR COMPONENTE: leakage prevention between sit and orifice')); 
                 verificar(safety_valve_spring)).
 
 verificar(safety_valve_spring) :- 
-                estado(safety_valve_spring, no), writeln('Putting spring and safety in the service') ; 
+                estado(safety_valve_spring, no), writeln('ACCION NECESARIA: Putting spring and safety in the service') ; 
                 
                 estado(safety_valve_spring, desconocido), 
                 (
-                    (estado(control_valve_sensors_blocked, no), writeln('Verificar safety valve spring')); 
+                    (estado(control_valve_sensors_blocked, no), writeln('VERIFICAR COMPONENTE: safety valve spring')); 
                     verificar(control_valve_sensors_blocked)
                 ).
 
 verificar(control_valve_sensors_blocked) :- 
-                estado(control_valve_sensors_blocked, ok), writeln('Cleaning and troubleshooting of the sensing pipes') ; 
+                estado(control_valve_sensors_blocked, ok), writeln('ACCION NECESARIA: Cleaning and troubleshooting of the sensing pipes') ; 
                 estado(control_valve_sensors_blocked, desconocido), 
                 ((estado(valve_status_closed, no), writeln('Verificar control valve sensors blocked')); 
                 verificar(valve_status_closed)).
                 
 verificar(valve_status_closed) :- 
-                estado(valve_status_closed, ok), writeln('Place the safety valve in open position') ; 
+                estado(valve_status_closed, ok), writeln('ACCION NECESARIA: Place the safety valve in open position') ; 
 
                 estado(valve_status_closed, desconocido),
                 (
@@ -80,7 +83,7 @@ verificar(valve_status_closed) :-
 % Verifciar si no hay que hacerlo aprecido al de threshold
 verificar(relief_valve_ok_with_10_percent_more_pressure) :- 
 
-                estado(relief_valve_ok_with_10_percent_more_pressure, ok), writeln('Safety function is appropriate') ; 
+                estado(relief_valve_ok_with_10_percent_more_pressure, ok), writeln('ACCION NECESARIA: Safety function is appropriate') ; 
 
                 estado(relief_valve_ok_with_10_percent_more_pressure, desconocido),
                 ((estado(safety_valve_has_continuous_evacuation, no), writeln('Verificar relief valve works correctly with +10% over regular pressure')); 
@@ -93,8 +96,8 @@ verificar(safety_valve_has_continuous_evacuation) :-
 
 % Rama CENTRAL derecha
 verificar(preventable_lackage_between_sit_and_orifice) :- 
-                estado( preventable_lackage_between_sit_and_orifice, ok ), writeln('Set the safety valve according to the instructions ');
-                estado( preventable_lackage_between_sit_and_orifice, no ), writeln('Replace Sit and Orifice and put the safety valve into circuit');
+                estado( preventable_lackage_between_sit_and_orifice, ok ), writeln('ACCION NECESARIA: Set the safety valve according to the instructions ');
+                estado( preventable_lackage_between_sit_and_orifice, no ), writeln('ACCION NECESARIA: Replace Sit and Orifice and put the safety valve into circuit');
                 (
                 estado( preventable_lackage_between_sit_and_orifice, desconocido ), 
                 ((estado(safety_spring_effective, ok ), writeln('Verificar preventable lackage between sit and orifice'));
@@ -102,7 +105,7 @@ verificar(preventable_lackage_between_sit_and_orifice) :-
 
 verificar(safety_spring_effective) :- 
                 
-                estado( safety_spring_effective, no ), writeln('Replace the safety spring in the service');
+                estado( safety_spring_effective, no ), writeln('ACCION NECESARIA: Replace the safety spring in the service');
                 
                 (estado( safety_spring_effective, desconocido ), 
                 (
@@ -153,7 +156,7 @@ verificar(leakage_fixed_with_wrench_at_joints) :-
         )
     ).
 verificar(gas_leakage_at_joint) :- 
-    estado(gas_leakage_at_joint, no), writeln('The safety valve joint are free of gas leakage.');
+    % estado(gas_leakage_at_joint, no), writeln('The safety valve joint are free of gas leakage.');
     estado(gas_leakage_at_joint, desconocido), writeln('Verificar gas leakage at joint').
 
 
@@ -168,23 +171,23 @@ estado( safety_valve_components_having_effects, no ).
 
 % RAMA CENTRAL izqueirda
 estado(piloto, ok).
-estado(leakage_prevention_between_sit_and_orifice, desconocido).
-estado(safety_valve_spring, desconocido).
-estado(control_valve_sensors_blocked, desconocido).
-estado(valve_status_closed, desconocido).
-estado(relief_valve_ok_with_10_percent_more_pressure, desconocido).
-estado(safety_valve_has_continuous_evacuation, desconocido).
+estado(leakage_prevention_between_sit_and_orifice, no).
+estado(safety_valve_spring, ok).
+estado(control_valve_sensors_blocked, no).
+estado(valve_status_closed, ok).
+estado(relief_valve_ok_with_10_percent_more_pressure, ok).
+estado(safety_valve_has_continuous_evacuation, no).
 
 
 % RAMA CENTRAL izqueirda
-estado( preventable_lackage_between_sit_and_orifice, desconocido ).
-estado( safety_spring_effective, desconocido ).
-estado( control_and_pressuere_sensor_pipes_blocked, desconocido ).
-estado( line_gas_pressure_appropriate, desconocido ).
+estado( preventable_lackage_between_sit_and_orifice, ok ).
+estado( safety_spring_effective, ok ).
+estado( control_and_pressuere_sensor_pipes_blocked, ok ).
+estado( line_gas_pressure_appropriate, ok ).
 
 
 %RAMA DERECHA
-estado(leakage_fixed_with_wrench_at_joints, desconocido).
-estado(gas_leakage_at_joint, desconocido).
+estado(leakage_fixed_with_wrench_at_joints, no).
+estado(gas_leakage_at_joint, ok).
 
 
